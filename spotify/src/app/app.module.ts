5df1613f2bed0b44d79bf280e4176e3eaf623ca0
Camm66 <Cam.morales93@gmail.com>
2018-10-05 22:26:08 -0500

    Initial Commit

---
+++ spotify/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { LocationStrategy, HashLocationStrategy, APP_BASE_HREF } from '@angular/common';
+import { AppComponent } from './app.component';
+import { AlbumComponent } from './album/album.component';
+import { ArtistComponent } from './artist/artist.component';
+import { SearchComponent } from './search/search.component';
+import { TrackComponent } from './track/track.component';
+import { SPOTIFY_PROVIDERS } from './spotify.service';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'search', pathMatch: 'full' },
+  { path: 'search', component: SearchComponent },
+  { path: 'artists/:id', component: ArtistComponent },
+  { path: 'tracks/:id', component: TrackComponent },
+  { path: 'albums/:id', component: AlbumComponent },
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    AlbumComponent,
+    ArtistComponent,
+    TrackComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
